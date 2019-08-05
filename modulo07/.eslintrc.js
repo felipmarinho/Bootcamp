3395f51d96af7ec4f11eb7f0756225a8f98b57da
Felipe Marinho <felipmarinho@gmail.com>
2019-08-04 21:35:07 -0300

    Refactoring

--- modulo07/.eslintrc.js
+++ modulo07/.eslintrc.js
@@ -1,30 +1,30 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+    env: {
+        browser: true,
+        es6: true
     },
-    "extends": [
-      "airbnb",
+    extends: [
+      'airbnb',
       'prettier',
       'prettier/react'
     ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly'
     },
     parser: 'babel-eslint',
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
         },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
+        ecmaVersion: 2018,
+        sourceType: 'module'
     },
-    "plugins": [
-        "react",
+    plugins: [
+        'react',
         'prettier'
     ],
-    "rules": {
+    rules: {
       'prettier/prettier': 'error',
       'react/jsx-filename-extension': [
         'warn',
