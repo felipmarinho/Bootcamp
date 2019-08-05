0ef3f005caf8b11869e9b441321fc225fce40ca7
Felipe Marinho <felipmarinho@gmail.com>
2019-08-05 19:08:06 -0300

    Getting products of API

--- modulo07/src/Pages/Home/index.js
+++ modulo07/src/Pages/Home/index.js
@@ -1,109 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import { MdAddShoppingCart } from 'react-icons/md';
+import { formatPrice } from '../../util/format';
+import api from '../../services/api';
 
 import { ProductList } from './styles';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-flex-2018-rn-masculino/26/D12-9471-026/D12-9471-026_detalhe1.jpg?resize=280:280"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$179,90</span>
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" />3
-          </div>
+  async componentDidMount() {
+    const response = await api.get('products');
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+    this.setState({ products: data });
+  }
 
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-flex-2018-rn-masculino/26/D12-9471-026/D12-9471-026_detalhe1.jpg?resize=280:280"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$179,90</span>
+  render() {
+    const { products } = this.state;
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" />3
-          </div>
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-flex-2018-rn-masculino/26/D12-9471-026/D12-9471-026_detalhe1.jpg?resize=280:280"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$179,90</span>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#FFF" />3
+              </div>
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" />3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-flex-2018-rn-masculino/26/D12-9471-026/D12-9471-026_detalhe1.jpg?resize=280:280"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$179,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" />3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-flex-2018-rn-masculino/26/D12-9471-026/D12-9471-026_detalhe1.jpg?resize=280:280"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$179,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" />3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-flex-2018-rn-masculino/26/D12-9471-026/D12-9471-026_detalhe1.jpg?resize=280:280"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$179,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" />3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
