20c8443753627094cbe43e04e23d0ae4337712a8
Felipe Marinho <felipmarinho@gmail.com>
2019-08-10 08:37:27 -0300

    Using hook useState

--- modulo08/src/App.js
+++ modulo08/src/App.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  return <h1>Hello world</h1>;
+  const [techs, setTech] = useState(['ReactJS', 'React Native']);
+  const [newTech, setNewTech] = useState('');
+
+  function handleAdd() {
+    setTech([...techs, newTech]);
+    setNewTech('');
+  }
+
+  return (
+    <>
+      <ul>
+        {techs.map(tech => (
+          <li key={tech}>{tech}</li>
+        ))}
+      </ul>
+
+      <input value={newTech} onChange={e => setNewTech(e.target.value)} />
+      <button type="button" onClick={handleAdd}>
+        Adicionar
+      </button>
+    </>
+  );
 }
 
 export default App;
